510f3cd3e6fa88294c873ec31b8b6cedf2b57834
Mongkud Klungpornkun <mongkud167@gmail.com>
2015-09-10 17:40:43 +0700

    Truly Complete K-Fold

--- Wind Lab/3 Fold Cross Validation.R
+++ Wind Lab/3 Fold Cross Validation.R
@@ -1,29 +1,26 @@
 library("neuralnet")
+windData = read.csv("windCleanedH.csv",TRUE)
 
 #########
 #SETTING
 #########
 k = 3
-thresholdRange = (10:3)
+thresholdRange = (10:1)
 saveToCSV = TRUE
 plot = FALSE
 printTrace = TRUE
 printResult = FALSE
 
-for(hiddenNode in (1:5)) {
+
+for(hiddenNode in (8:8)) {
   
   fileCSV = data.frame()
-  for(z in 1:7)
+  for(z in 1:10)
     fileCSV[1,z] = NA
-  colnames(fileCSV) = c("Threshold","NN MAPE1","NN MAPE2","NN MAPE3","Predict MAPE","SD Predict MAPE","Elapsed Time")
-  row = 1
+  colnames(fileCSV) = c("Threshold","NN MAPE1","PD MAPE1","NN MAPE2","PD MAPE2","NN MAPE3","PD MAPE3","Predict MAPE","SD Predict MAPE","Elapsed Time")
   
-  for(cnr in (0:7)) {
-    
-    #########
-    #SETTING
-    #########
-    firstRun = TRUE
+  defRow = 1
+  for(cnr in (5:5)) {
     
     inputList = c()
     inputList = append(inputList,"WIND_SPEED")
@@ -34,64 +31,72 @@ for(hiddenNode in (1:5)) {
     inputNames = paste("trainingData$",inputList,sep = "")
     f = as.formula(paste("trainingData$POWER ~",paste(inputNames,collapse = "+")))
     
-    
-    elapsedTime = 0
-    if(firstRun)s_weights = NULL
-    if(printTrace)cat("Threshold : ")
-    windData = read.csv("windCleanedH.csv",TRUE)
-    for(th in thresholdRange){
-      ###########
-      # SETTING
-      ###########
-      thresholdEquation = 2^th
+    for(xFold in (0:(k-1))){
       
-      if(printTrace)cat(thresholdEquation," ")
-      error_sum = c()
-      fileCSV[row,"Threshold"] = thresholdEquation
-      for(i in 0:(k-1)){
-        ##############
-        # Partition
-        ##############
-        testing_range = c(1:(nrow(windData)%/%k)) + i * (nrow(windData)%/%k)
-        trainingData = windData[-testing_range,append(c("POWER","MIN_TIME"),inputList)]
-        testingData = windData[testing_range,append(c("POWER","MIN_TIME"),inputList)]
-        trainingData$POWER = sqrt(trainingData$POWER)
-        testingData$POWER = sqrt(testingData$POWER)
+      row = defRow
+      elapsedTime = 0
+      s_weights = NULL
+      if(printTrace)cat("Fold-",(xFold+1)," ",sep = "")
+      
+      ##############
+      # Partition
+      ##############
+      testing_range = c(1:(nrow(windData)%/%k)) + xFold * (nrow(windData)%/%k)
+      trainingData = windData[-testing_range,append(c("POWER","MIN_TIME"),inputList)]
+      testingData = windData[testing_range,append(c("POWER","MIN_TIME"),inputList)]
+      trainingData$POWER = sqrt(trainingData$POWER)
+      testingData$POWER = sqrt(testingData$POWER)
+      
+      #############
+      # NORMALIZE Training data
+      #############
+      
+      if("HOUR_TIME" %in% inputList) {
+        HOUR_TIMEu = mean(trainingData$HOUR_TIME)
+        HOUR_TIMEsd = sd(trainingData$HOUR_TIME)
+        trainingData$HOUR_TIME = scale(trainingData$HOUR_TIME/100)
+      }
+      
+      if("WIND_MOV" %in% inputList) {
+        WIND_MOVu = mean(trainingData$WIND_MOV)
+        WIND_MOVsd = sd(trainingData$WIND_MOV)
+        trainingData$WIND_MOV = scale(trainingData$WIND_MOV)
+      }
+      
+      if("SPD_MOV" %in% inputList) {
+        SPD_MOVu = mean(trainingData$SPD_MOV)
+        SPD_MOVsd = sd(trainingData$SPD_MOV)
+        trainingData$SPD_MOV = scale(trainingData$SPD_MOV)
+      }
+      
+      # Test Data
+      if("HOUR_TIME" %in% inputList)testingData$HOUR_TIME = (testingData$HOUR_TIME-HOUR_TIMEu)/HOUR_TIMEsd
+      if("WIND_MOV" %in% inputList)testingData$WIND_MOV = (testingData$WIND_MOV-WIND_MOVu)/WIND_MOVsd
+      if("SPD_MOV" %in% inputList)testingData$SPD_MOV = (testingData$SPD_MOV-SPD_MOVu)/SPD_MOVsd
+      
+      if(printTrace)cat("Threshold : ")
+      for(th in thresholdRange){
         
-        #############
-        # NORMALIZE Training data
-        #############
+        thresholdEquation = 2^th
         
-        if("HOUR_TIME" %in% inputList) {
-          HOUR_TIMEu = mean(trainingData$HOUR_TIME)
-          HOUR_TIMEsd = sd(trainingData$HOUR_TIME)
-          trainingData$HOUR_TIME = scale(trainingData$HOUR_TIME/100)
-        }
-        
-        if("WIND_MOV" %in% inputList) {
-          WIND_MOVu = mean(trainingData$WIND_MOV)
-          WIND_MOVsd = sd(trainingData$WIND_MOV)
-          trainingData$WIND_MOV = scale(trainingData$WIND_MOV)
-        }
-        
-        if("SPD_MOV" %in% inputList) {
-          SPD_MOVu = mean(trainingData$SPD_MOV)
-          SPD_MOVsd = sd(trainingData$SPD_MOV)
-          trainingData$SPD_MOV = scale(trainingData$SPD_MOV)
-        }
-        
+        if(printTrace)cat(thresholdEquation," ")
+        fileCSV[row,"Threshold"] = thresholdEquation
         
         ##################
         # Neural Network
         ##################
         startTime = proc.time()
-        if(firstRun) { 
-          net = neuralnet(f, trainingData[,inputList], hidden = hiddenNode, threshold = thresholdEquation,stepmax = 100000)
-          firstRun = FALSE
+        if(th == thresholdRange[1]) { 
+          net = neuralnet(f, trainingData[,inputList], hidden = hiddenNode, threshold = thresholdEquation, stepmax = 1000000)
         }
-        else
-          net = neuralnet(f, trainingData[,inputList], hidden = hiddenNode, threshold = thresholdEquation,startweights = s_weights,stepmax = 100000)
+        else {
+          net = neuralnet(f, trainingData[,inputList], hidden = hiddenNode, threshold = thresholdEquation, startweights = s_weights,stepmax = 1000000)
+        }
         elapsedTime = (proc.time() - startTime) + elapsedTime
+        if(is.na(fileCSV[row,"Elapsed Time"]))
+          fileCSV[row,"Elapsed Time"] = as.double(elapsedTime[3])
+        else
+          fileCSV[row,"Elapsed Time"] = fileCSV[row,"Elapsed Time"] + as.double(elapsedTime[3])
         
         if(plot) {
           plotData = trainingData
@@ -105,7 +110,7 @@ for(hiddenNode in (1:5)) {
           l = lm(y ~ x)
           abline(l,lwd = 1)
           grid(20,20)
-          title(paste("Data 2011-2013 MAPE",(i+1)))
+          title(paste("Data 2011-2013 MAPE",(xFold+1),"Threshold",thresholdEquation))
         }
         
         nnData = as.data.frame(unlist(net["net.result"]))
@@ -114,16 +119,12 @@ for(hiddenNode in (1:5)) {
         colnames(nnData) = c("POWER","WIND_SPEED")
         if(plot)points(nnData$WIND_SPEED,nnData$POWER,col="green")
         s_weights = net["weights"]
-        fileCSV[row,paste("NN MAPE",(i+1),sep="")] = sum(abs((nnData$POWER^2 - trainingData$POWER^2)/trainingData$POWER^2))/nrow(trainingData)
-        if(printResult)print(paste("NN MAPE",(i+1),":",fileCSV[row,paste("NN MAPE",(i+1),sep="")]))
+        fileCSV[row,paste("NN MAPE",(xFold+1),sep="")] = sum(abs((nnData$POWER^2 - trainingData$POWER^2)/trainingData$POWER^2))/nrow(trainingData)
+        if(printResult)print(paste("NN MAPE",(xFold+1),":",fileCSV[row,paste("NN MAPE",(xFold+1),sep="")]))
         
         #################################
         # PREDICT BETA
         #################################
-        
-        if("HOUR_TIME" %in% inputList)testingData$HOUR_TIME = (testingData$HOUR_TIME-HOUR_TIMEu)/HOUR_TIMEsd
-        if("WIND_MOV" %in% inputList)testingData$WIND_MOV = (testingData$WIND_MOV-WIND_MOVu)/WIND_MOVsd
-        if("SPD_MOV" %in% inputList)testingData$SPD_MOV = (testingData$SPD_MOV-SPD_MOVu)/SPD_MOVsd
         
         predictData = as.data.frame(unlist(compute(net, covariate = testingData[,inputList])["net.result"]))
         rownames(predictData) = NULL
@@ -133,26 +134,28 @@ for(hiddenNode in (1:5)) {
           points(testingData$WIND_SPEED,testingData$POWER,col="red")
           points(predictData$WIND_SPEED,predictData$POWER,col="green")
         }
-        error_sum = append(error_sum,abs((predictData$POWER^2 - testingData$POWER^2)/testingData$POWER^2))
-      }
-      mape = mean(error_sum)
-      SDmape = sd(error_sum)
-      fileCSV[row,"Predict MAPE"] = mape
-      fileCSV[row,"SD Predict MAPE"] = SDmape
-      fileCSV[row,"Elapsed Time"] = as.double(elapsedTime[3])
-      if(printResult) {
-        print(paste("Predict MAPE :",mape))
-        print(paste("SD Predict MAPE :",SDmape))
-        print(paste("Elapsed Time :",as.double(elapsedTime[3])))
-      }
-      row = row + 1
-    }
+        fileCSV[row,paste("PD MAPE",(xFold+1),sep="")] = mean(abs((predictData$POWER^2 - testingData$POWER^2)/testingData$POWER^2))
+        if(printResult)print(paste("PD MAPE",(xFold+1),":",fileCSV[row,paste("PD MAPE",(xFold+1),sep="")]))
+        if(xFold == (k-1)) {
+          fileCSV[row,"Predict MAPE"] = mean(as.double(fileCSV[row,paste("PD MAPE",(1:k),sep="")]))
+          fileCSV[row,"SD Predict MAPE"] = sd(as.double(fileCSV[row,paste("PD MAPE",(1:k),sep="")]))
+          if(printResult) {
+            print(paste("Predict MAPE :",fileCSV[row,"Predict MAPE"]))
+            print(paste("SD Predict MAPE :",fileCSV[row,"SD Predict MAPE"]))
+          }
+        }
+        row = row + 1
+      } # for(th in thresholdRange)
+      
+    } # for(xFold in (0:(k-1)))
+    
     fileCSV[row,1] = paste(substr(inputList[1:length(inputList)],1,1),collapse = "")
     row = row + 1
+    defRow = row
   }
   if(saveToCSV)
     write.csv(fileCSV,
-              paste("SummaryHid",hiddenNode,".csv",sep=""),
+              paste("SummaryHid",hiddenNode,"BEST.csv",sep=""),
               row.names = FALSE)
   cat("\n","COMPLETE for Hidden",hiddenNode,"\n")
   hiddenNode = hiddenNode + 1
